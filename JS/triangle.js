da20a8bf5b0af47018e4cd1f15b53a21ca3bc3e5
James Mutahi <mutahijames0@gmail.com>
2019-04-28 15:13:45 +0300

    Add Javascript: display the output in the form of an alert

--- JS/triangle.js
+++ JS/triangle.js
@@ -3,7 +3,7 @@ function myFunction() {
   var L1 = parseInt(document.getElementById("L1").value);
   var L2 = parseInt(document.getElementById("L2").value);
   var L3 = parseInt(document.getElementById("L3").value);
-  
+
   // Add the input to an array
   var array = [L1, L2, L3];
 
@@ -13,5 +13,7 @@ function myFunction() {
   // condition for lengths to be greater than 0
   if (L1 <= 0 || L2 <= 0 || L3 <= 0) {
     text = " not a triangle. A triangle must have a length greater than 0."
-}
+  }
+
+  alert("The values you entered are:" + " [" + array.join() + "] " + " This is" + text);
 }
