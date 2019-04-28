fbbd2acb51eb5a893103c292381f47dbf616c8ec
James Mutahi <mutahijames0@gmail.com>
2019-04-28 15:08:28 +0300

    Add Javascript: Get input then add to array

---
+++ JS/triangle.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  var L1 = parseInt(document.getElementById("L1").value);
+  var L2 = parseInt(document.getElementById("L2").value);
+  var L3 = parseInt(document.getElementById("L3").value);
+  
+  var array = [L1, L2, L3];
+}
